88a4b271f2646470826e26f89c0d740bb5a40013
Devin Moon <devmoon@umd.edu>
2023-02-23 12:32:35 -0500

    Minor adjustments

--- DashboardPOC/src/components/PatientTable/PatientsTable.js
+++ DashboardPOC/src/components/PatientTable/PatientsTable.js
@@ -4,20 +4,20 @@ import { useState, useEffect } from 'react';
 
 
 const columns = [
-    { field: "id", headerName: "ID", type: 'number', align: 'left', headerAlign: 'left'},
-    { field: 'fname', headerName: 'First name', flex: 1 },
-    { field: 'lname', headerName: 'Last name', flex: 1 },
+    { field: "id", headerName: "ID", type: 'number', align: 'left', headerAlign: 'left' },
+    { field: 'fname', headerName: 'First Name', flex: 1 },
+    { field: 'lname', headerName: 'Last Name', flex: 1 },
     { field: 'cholesterol', headerName: 'Cholesterol', type: 'number', flex: 1, align: 'left', headerAlign: 'left' },
-    { field: 'age', headerName: 'Age', type: 'number', flex: 1, align: 'left', headerAlign: 'left'}
+    { field: 'age', headerName: 'Age', type: 'number', flex: 1, align: 'left', headerAlign: 'left' }
 ];
   
 const defaultRows = [
-    { id: 1, fname: 'FILLER', lname: 'Doe', age: 35, cholesterol:'113.7' },
-    { id: 2, fname: 'FILLER', lname: 'Johnson', age: 25, cholesterol:'143.7' },
-    { id: 3, fname: 'FILLER', lname: 'Shell', age: 17, cholesterol:'103.7' },
-    { id: 4, fname: 'FILLER', lname: 'Smith', age: 9, cholesterol:'221.7' },
-    { id: 5, fname: 'FILLER', lname: 'Barb', age: 63, cholesterol:'123.7' },
-    { id: 6, fname: 'FILLER', lname: 'Chip', age: 40, cholesterol:'172.7' }
+    { id: 1, fname: 'FILLER', lname: 'Doe', age: 35, cholesterol: '113.7' },
+    { id: 2, fname: 'FILLER', lname: 'Johnson', age: 25, cholesterol: '143.7' },
+    { id: 3, fname: 'FILLER', lname: 'Shell', age: 17, cholesterol: '103.7' },
+    { id: 4, fname: 'FILLER', lname: 'Smith', age: 9, cholesterol: '221.7' },
+    { id: 5, fname: 'FILLER', lname: 'Barb', age: 63, cholesterol: '123.7' },
+    { id: 6, fname: 'FILLER', lname: 'Chip', age: 40, cholesterol: '172.7' }
 ];
 
 const tableStyle = {
